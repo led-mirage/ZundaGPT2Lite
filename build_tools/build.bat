66729ea639ea37aaa40e2d2370c473f728c60f6a
led-mirage <139528700+led-mirage@users.noreply.github.com>
2026-02-09 17:53:43 +0900

    ビルドバッチ修正

--- build_tools/build.bat
+++ build_tools/build.bat
@@ -9,8 +9,8 @@ set VENV_MAKE_VERSION=venv\Scripts\pyivf-make_version.exe
 
 if exist build rmdir /s /q build
 
-"%VENV_PYTHON%" tools/update_version_yaml.py
-"%VENV_PYTHON%" tools/update_app_manifest.py
+"%VENV_PYTHON%" build_tools/update_version_yaml.py
+"%VENV_PYTHON%" build_tools/update_app_manifest.py
 
 "%VENV_MAKE_VERSION%" ^
     --source-format yaml ^
@@ -21,7 +21,7 @@ if exist build rmdir /s /q build
     --onefile ^
     --noconsole ^
     --paths=./app ^
-    --add-data "app/html;html" ^
+    --add-data "./app/html;html" ^
     --name ZundaGPT2Lite ^
     --icon "assets/ZundaGPT2.ico" ^
     --splash "assets/ZundaGPT2_splash.png" ^
--- build_tools/build.sh
+++ build_tools/build.sh
@@ -6,7 +6,7 @@ cd "$(dirname "$0")/.."
 rm -rf build dist
 pyinstaller --onefile --noconsole \
   --paths=./app \
-  --add-data "app/html:html" \
+  --add-data "./app/html:html" \
   --add-data "assets/ZundaGPT2Lite-Icon64.png:assets" \
   --name ZundaGPT2Lite \
   --splash assets/ZundaGPT2_splash.png \
